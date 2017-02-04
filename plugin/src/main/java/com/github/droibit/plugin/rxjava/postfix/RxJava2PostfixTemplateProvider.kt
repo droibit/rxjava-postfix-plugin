f28fd2012bdaee9420a24a81da7a16ea4fecce3c
Shinya Kumgai <roomful.rooms@gmail.com>
2017-02-05 02:10:53 +0900

    Added SingleJustTemplate

--- plugin/src/main/java/com/github/droibit/plugin/rxjava/postfix/RxJava2PostfixTemplateProvider.kt
+++ plugin/src/main/java/com/github/droibit/plugin/rxjava/postfix/RxJava2PostfixTemplateProvider.kt
@@ -3,6 +3,7 @@ package com.github.droibit.plugin.rxjava.postfix
 import com.github.droibit.plugin.rxjava.postfix.templates.ObservableErrorTemplate
 import com.github.droibit.plugin.rxjava.postfix.templates.ObservableFromIterableOrArrayTemplate
 import com.github.droibit.plugin.rxjava.postfix.templates.ObservableJustTemplate
+import com.github.droibit.plugin.rxjava.postfix.templates.SingleJustTemplate
 import com.intellij.codeInsight.template.postfix.templates.JavaPostfixTemplateProvider
 
 class RxJava2PostfixTemplateProvider : JavaPostfixTemplateProvider() {
@@ -10,7 +11,8 @@ class RxJava2PostfixTemplateProvider : JavaPostfixTemplateProvider() {
     private val templates = hashSetOf(
             ObservableFromIterableOrArrayTemplate(),
             ObservableJustTemplate(),
-            ObservableErrorTemplate()
+            ObservableErrorTemplate(),
+            SingleJustTemplate()
     )
 
     override fun getTemplates() = templates
--- plugin/src/main/java/com/github/droibit/plugin/rxjava/postfix/utils/RxJavaClassName.kt
+++ plugin/src/main/java/com/github/droibit/plugin/rxjava/postfix/utils/RxJavaClassName.kt
@@ -2,5 +2,6 @@ package com.github.droibit.plugin.rxjava.postfix.utils
 
 enum class RxJavaClassName(val className: String) {
     OBSERVABLE("io.reactivex.Observable"),
-    FLOWABLE("io.reactivex.Flowable")
+    FLOWABLE("io.reactivex.Flowable"),
+    SINGLE("io.reactivex.Single")
 }
