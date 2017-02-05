511afbb97ef597c2173c4dd1655b759d3f2f94c9
Shinya Kumgai <roomful.rooms@gmail.com>
2017-02-05 18:25:49 +0900

    Fixed selector of SingleErrorTemplate

--- plugin/src/main/java/com/github/droibit/plugin/rxjava/postfix/templates/SingleErrorTemplate.kt
+++ plugin/src/main/java/com/github/droibit/plugin/rxjava/postfix/templates/SingleErrorTemplate.kt
@@ -1,21 +1,19 @@
 package com.github.droibit.plugin.rxjava.postfix.templates
 
 import com.github.droibit.plugin.rxjava.postfix.utils.RxJavaClassName.SINGLE
-import com.github.droibit.plugin.rxjava.postfix.utils.RxJavaPostfixTemplatesUtils.IS_NON_NULL
 import com.github.droibit.plugin.rxjava.postfix.utils.RxJavaPostfixTemplatesUtils.getStaticPrefix
 import com.intellij.codeInsight.template.Template
 import com.intellij.codeInsight.template.Template.Property.USE_STATIC_IMPORT_IF_POSSIBLE
 import com.intellij.codeInsight.template.TemplateManager
 import com.intellij.codeInsight.template.postfix.templates.StringBasedPostfixTemplate
-import com.intellij.codeInsight.template.postfix.util.JavaPostfixTemplatesUtils.IS_NON_VOID
-import com.intellij.codeInsight.template.postfix.util.JavaPostfixTemplatesUtils.selectorAllExpressionsWithCurrentOffset
-import com.intellij.openapi.util.Conditions.and
+import com.intellij.codeInsight.template.postfix.util.JavaPostfixTemplatesUtils.IS_THROWABLE
+import com.intellij.codeInsight.template.postfix.util.JavaPostfixTemplatesUtils.selectorTopmost
 import com.intellij.psi.PsiElement
 
 class SingleErrorTemplate : StringBasedPostfixTemplate(
         "serror",
         "Single.error(expr)",
-        selectorAllExpressionsWithCurrentOffset(and(IS_NON_NULL, IS_NON_VOID))) {
+        selectorTopmost(IS_THROWABLE)) {
 
     override fun createTemplate(manager: TemplateManager, templateString: String): Template {
         return super.createTemplate(manager, templateString).apply {
